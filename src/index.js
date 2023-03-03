9aef330a97bf7e6af6416b1a65019dafa27ea9bb
MinhSon <nguyenminhson102002@gmail.com>
2023-03-04 00:07:16 +0700

    add tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './components/GlobalStyles';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Toast from './components/Toast';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
