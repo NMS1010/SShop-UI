9094469a07f3c77dba22b2a3c009a07f6df45e71
MinhSon <nguyenminhson102002@gmail.com>
2023-03-04 00:08:47 +0700

    create ProductCard component

---
+++ src/components/ProductCard/index.js
@@ -0,0 +1,101 @@
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+const ProductCard = ({ product }) => {
+    const [hide, setHide] = useState(true);
+    return (
+        <div
+            onMouseEnter={() => setHide(product?.quantity === 0 ? true : false)}
+            onMouseLeave={() => setHide(true)}
+            className="w-full max-w-lg m-auto text-2xl border text-center shadow dark:bg-gray-800 dark:border-gray-700 border-gray-200 rounded-lg"
+        >
+            <div className="bg-gray-100 relative overflow-hidden">
+                <div
+                    className={
+                        hide
+                            ? 'ease-out duration-300 m-auto absolute right-0 left-0 bg-gray-300 hover:bg-cyan-300'
+                            : 'ease-out duration-300 m-auto absolute right-0 left-0 bg-gray-300 hover:bg-cyan-300 top-0'
+                    }
+                    style={{
+                        top: '-6rem',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        padding: '1rem',
+                        cursor: 'pointer',
+                    }}
+                >
+                    <FontAwesomeIcon icon={faHeart} />
+                    <p className="ml-2 -mt-1 mb-0">Yêu thích</p>
+                </div>
+
+                <img
+                    className="m-auto rounded-t-lg w-full cursor-pointer"
+                    src={`${process.env.REACT_APP_HOST}${product?.imagePath}`}
+                />
+                {product?.quantity === 0 && (
+                    <span className="absolute top-0 left-0 bg-gray-400 p-3 rounded-lg">Out of Stock</span>
+                )}
+                <div
+                    className={
+                        hide
+                            ? 'ease-out duration-300 m-auto absolute right-0 left-0 bg-gray-300 hover:bg-cyan-300'
+                            : 'ease-out duration-300 m-auto absolute right-0 left-0 bg-gray-300 hover:bg-cyan-300 bottom-0'
+                    }
+                    style={{
+                        bottom: '-6rem',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        padding: '1rem',
+                        cursor: 'pointer',
+                    }}
+                >
+                    <FontAwesomeIcon icon={faCartShopping} />
+                    <p className="ml-2 -mt-1 mb-0">Thêm vào giỏ hàng</p>
+                </div>
+            </div>
+            <div>
+                <ul className="list-disc flex justify-evenly text-2sm mt-3 mb-3">
+                    <li className=" cursor-pointer hover:text-cyan-500 ease-in duration-300">{product?.brandName}</li>
+                    <li className=" cursor-pointer hover:text-cyan-500 ease-in duration-300">
+                        {product?.categoryName}
+                    </li>
+                </ul>
+                <div className="text-3xl font-bold">
+                    <span className="cursor-pointer hover:text-cyan-500 ease-in duration-300">{product?.name}</span>
+                </div>
+                <p className="text-cyan-500 mt-3">{product?.price} VND</p>
+                <div class="flex items-center justify-center mt-2.5 mb-3">
+                    <span class="bg-blue-100 text-blue-800 text-2sm font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                        {product?.averageRating || 0}
+                    </span>
+                    {Array.from(Array(5)).map((val) => {
+                        return val < product?.averageRating ? (
+                            <svg
+                                aria-hidden="true"
+                                class="h-8 text-yellow-300"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                            </svg>
+                        ) : (
+                            <svg
+                                aria-hidden="true"
+                                class="h-8 "
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                            </svg>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
