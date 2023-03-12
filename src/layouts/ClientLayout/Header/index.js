a10c3c69d0b485859a4b3dccf150966de500c710
MinhSon <nguyenminhson102002@gmail.com>
2023-03-12 11:21:43 +0700

    fix Header

--- src/layouts/ClientLayout/Header/index.js
+++ src/layouts/ClientLayout/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     let inActive =
         'text-3xl block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-cyan-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700';
     return (
-        <header className="border-b-2 mb-16">
+        <header className="border-b-2 mb-16 fixed top-0 left-0 right-0 z-10">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="/" className="flex items-center lg:order-1">
